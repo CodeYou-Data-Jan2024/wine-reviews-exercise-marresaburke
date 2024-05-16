9735994b955aae950bb584d40c93b579759bb847
marresaburke <marresa.burke@gmail.com>
2024-05-16 15:41:31 -0400

    Having issues with importing pandas on machine and git hub. Hopefully this works. I was proud of myself for how I figured this out.

--- reviews.py
+++ reviews.py
@@ -1,2 +1,23 @@
-# add your code here
+import pandas as pd
+
+#Read in CSV file
+wine_reviews = pd.read_csv('.../data/winemag-data-130k-v2.csv.zip', index_col = 0)
+
+#Find how many reviews per country
+data_wine = wine_reviews.country.value_counts()
+
+#Find the average points per country
+country_avg = wine_reviews.groupby('country').points.mean().round(decimals=1) 
+country_avg
+
+#Build new dataframe with data
+
+compliled = pd.DataFrame({'count': data_wine, 'points': country_avg})
+final_data = compliled.sort_values('count', ascending= 0)
+final_data
+
+#Save new dataframe to new csv file
+final_data.to_csv('reviews-per-country.csv')
+
+
 
