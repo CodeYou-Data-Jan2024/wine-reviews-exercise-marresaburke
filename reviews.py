85247d1de4afe4fe8f5cb77baa0478b2b400ae75
marresaburke <marresa.burke@gmail.com>
2024-05-19 19:06:41 -0400

    Fixed file path, and removed # off of lines of code

--- reviews.py
+++ reviews.py
@@ -3,25 +3,25 @@ import pandas as pd
 #Read in CSV file
 
 
-wine_reviews = pd.read_csv("../data/winemag-data-130k-v2.csv", index_col = 0)
-#wine_reviews
+wine_reviews = pd.read_csv("data/winemag-data-130k-v2.csv", index_col = 0)
+wine_reviews
 #Find how many reviews per country
-#data_wine = wine_reviews.country.value_counts()
-#data_wine
+data_wine = wine_reviews.country.value_counts()
+data_wine
 
 #Find the average points per country
-#country_avg = wine_reviews.groupby('country').points.mean().round(decimals=1) 
+country_avg = wine_reviews.groupby('country').points.mean().round(decimals=1) 
 
 #Build new dataframe with data
 
-#compliled = pd.DataFrame({'count': data_wine, 'points': country_avg})
+compliled = pd.DataFrame({'count': data_wine, 'points': country_avg})
 
-#final_data = compliled.sort_values('count', ascending= 0)
+final_data = compliled.sort_values('count', ascending= 0)
 
 
 #Save new dataframe to new csv file
 
-#final_data.to_csv('../data/reviews-per-country.csv')
+final_data.to_csv('data/reviews-per-country.csv')
 
 
 
